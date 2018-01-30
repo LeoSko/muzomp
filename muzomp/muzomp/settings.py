1ace40e50fd7acd84201000250ed01b337a6ca16
LeoSko <leosko94@gmail.com>
2018-01-31 01:21:20 +0300

    Moved package inside, idea project

--- muzomp/muzomp/settings.py
+++ muzomp/muzomp/settings.py
@@ -57,7 +57,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'muzomp.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     #     'default': {
@@ -75,7 +74,6 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,8 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
@@ -108,7 +104,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
